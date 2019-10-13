cd31d297a4fa14b1be4cc477e2a01f8ae525eaa1
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-10-13 16:31:08 +0300

    Ignore limitParallelism test as we have to decide whether we want to fix limiting dispatcher at all

--- kotlinx-coroutines-core/jvm/test/scheduling/BlockingCoroutineDispatcherStressTest.kt
+++ kotlinx-coroutines-core/jvm/test/scheduling/BlockingCoroutineDispatcherStressTest.kt
@@ -22,26 +22,52 @@ class BlockingCoroutineDispatcherStressTest : SchedulerTestBase() {
     private val concurrentWorkers = AtomicInteger(0)
 
     @Test
-    fun testLimitParallelism() = runBlocking {
-        val limitingDispatcher = blockingDispatcher(CORES_COUNT)
-        val iterations = 50_000 * stressTestMultiplier
-        val tasks = (1..iterations).map {
-            async(limitingDispatcher) {
-                try {
-                    val currentlyExecuting = concurrentWorkers.incrementAndGet()
-                    observedConcurrency[currentlyExecuting] = true
-                    assertTrue(currentlyExecuting <= CORES_COUNT)
-                } finally {
-                    concurrentWorkers.decrementAndGet()
+    @Ignore
+    fun testLimitParallelismToOne() = runTest {
+        val limitingDispatcher = blockingDispatcher(1)
+        // Do in bursts to avoid OOM
+        repeat(100 * stressTestMultiplierSqrt) {
+            val iterations = 1_000 * stressTestMultiplierSqrt
+            val tasks = (1..iterations).map {
+                async(limitingDispatcher) {
+                    try {
+                        val currentlyExecuting = concurrentWorkers.incrementAndGet()
+                        observedConcurrency[currentlyExecuting] = true
+                        assertTrue(currentlyExecuting <= CORES_COUNT)
+                    } finally {
+                        concurrentWorkers.decrementAndGet()
+                    }
                 }
             }
+            tasks.forEach { it.await() }
+            for (i in CORES_COUNT + 1..CORES_COUNT * 2) {
+                require(i !in observedConcurrency.keys) { "Unexpected state: $observedConcurrency" }
+            }
+            checkPoolThreadsCreated(0..CORES_COUNT + 1)
         }
+    }
 
-        tasks.forEach { it.await() }
-        for (i in CORES_COUNT + 1..CORES_COUNT * 2) {
-            require(i !in observedConcurrency.keys) { "Unexpected state: $observedConcurrency" }
-        }
-        checkPoolThreadsCreated(CORES_COUNT..CORES_COUNT * 3)
+    @Test
+    @Ignore
+    fun testLimitParallelism() = runBlocking {
+            val limitingDispatcher = blockingDispatcher(CORES_COUNT)
+            val iterations = 50_000 * stressTestMultiplier
+            val tasks = (1..iterations).map {
+                async(limitingDispatcher) {
+                    try {
+                        val currentlyExecuting = concurrentWorkers.incrementAndGet()
+                        observedConcurrency[currentlyExecuting] = true
+                        assertTrue(currentlyExecuting <= CORES_COUNT)
+                    } finally {
+                        concurrentWorkers.decrementAndGet()
+                    }
+                }
+            }
+            tasks.forEach { it.await() }
+            for (i in CORES_COUNT + 1..CORES_COUNT * 2) {
+                require(i !in observedConcurrency.keys) { "Unexpected state: $observedConcurrency" }
+            }
+            checkPoolThreadsCreated(CORES_COUNT..CORES_COUNT * 3)
     }
 
     @Test
@@ -64,11 +90,8 @@ class BlockingCoroutineDispatcherStressTest : SchedulerTestBase() {
                     // These two will be stolen first
                     blockingTasks += async(blockingDispatcher) { blockingBarrier.await() }
                     blockingTasks += async(blockingDispatcher) { blockingBarrier.await() }
-
-
                     // Empty on CPU job which should be executed while blocked tasks are hang
                     cpuTasks += async(dispatcher) { cpuBarrier.await() }
-
                     // Block with next task. Block cores * 3 threads in total
                     blockingTasks += async(blockingDispatcher) { blockingBarrier.await() }
                 }
@@ -111,11 +134,9 @@ class BlockingCoroutineDispatcherStressTest : SchedulerTestBase() {
         repeat(iterations) {
             // Overwhelm global queue with external CPU tasks
             val cpuTasks = (1..CORES_COUNT).map { async(dispatcher) { while (true) delay(1) } }
-
             val barrier = CyclicBarrier(blockingLimit + 1)
             // Should eat all limit * 3 cpu without any starvation
             val tasks = (1..blockingLimit).map { async(blocking) { barrier.await() } }
-
             tasks.forEach { assertTrue(it.isActive) }
             barrier.await()
             tasks.joinAll()
